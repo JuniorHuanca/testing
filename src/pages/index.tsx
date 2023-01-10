cbd583693677fbbedefe0fb5d01c3c00a45bdd6f
elvis huanca <JHuanca_21@outlook.com>
2023-01-09 23:10:28 -0500

    dad

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function index() {
   const router = useRouter();
   // console.log(user);
   const handleCookieLogin = async () => {
-    const res = await axios.post(`/api/login`, user);
+    const res = await axios.post(`${process.env.RESTURL_PRODUCTS}/api/login`, user);
     // console.log(res);
     if (user) {
       const response = await axios.post(`${process.env.RESTURL_PRODUCTS}/users/register`, user);
